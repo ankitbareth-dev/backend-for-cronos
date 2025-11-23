e89a546d0c595414d07124ab7d7ecfea936ee920
ankitbareth-dev <ankitbareth.dev@gmail.com>
2025-11-23 13:40:56 +0530

    features: added express.json() middleware to parse json request bodies and added  a limit to json body added express.urlencoded() middaleware to Parses form-data from HTML forms

--- app.ts
+++ app.ts
@@ -2,6 +2,9 @@ import express, { Application, Request, Response } from "express";
 
 const app: Application = express();
 
+app.use(express.json({ limit: "1mb" }));
+app.use(express.urlencoded({ limit: "1mb", extended: true }));
+
 app.get("/", (req: Request, res: Response) => {
   res.status(200).json({
     message: "Api is working",
