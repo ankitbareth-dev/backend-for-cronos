443102de207eeff8e5ecbb7c2debcdbecd502ee4
ankitbareth-dev <ankitbareth.dev@gmail.com>
2025-11-23 13:51:26 +0530

    feat(security): add CORS, Helmet, and HPP middleware for enhanced API protection
    - Enabled CORS to allow secure cross-origin requests
    - Added Helmet to set security-related HTTP headers
    - Implemented HPP to prevent HTTP parameter pollution attacks

--- app.ts
+++ app.ts
@@ -1,9 +1,16 @@
 import express, { Application, Request, Response } from "express";
+import cors from "cors";
+import helmet from "helmet";
+import hpp from "hpp";
 
 const app: Application = express();
 
 app.use(express.json({ limit: "1mb" }));
 app.use(express.urlencoded({ limit: "1mb", extended: true }));
+
+app.use(cors());
+app.use(helmet());
+app.use(hpp());
 
 app.get("/", (req: Request, res: Response) => {
   res.status(200).json({
