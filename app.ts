a3cbc93e38c69d32527f3676065184127ddb4ecd
ankitbareth-dev <ankitbareth.dev@gmail.com>
2025-11-23 13:22:34 +0530

    health check-up route added

--- app.ts
+++ app.ts
@@ -1,5 +1,11 @@
-import express, { Application } from "express";
+import express, { Application, Request, Response } from "express";
 
 const app: Application = express();
+
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).json({
+    message: "Api is working",
+  });
+});
 
 export default app;
