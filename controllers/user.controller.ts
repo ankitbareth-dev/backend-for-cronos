5b026afb517365856c4ec3f9cee8acbe10d02ed3
ankitbareth-dev <ankitbareth.dev@gmail.com>
2025-11-25 18:03:27 +0530

    user controller added

---
+++ controllers/user.controller.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from "express";
+import * as userService from "../services/user.service";
+import { catchAsync } from "../utils/catchAsync";
+
+export const updateUser = catchAsync(async (req: Request, res: Response) => {
+  const userId = req.user.id;
+  const { name, email, oldPassword, newPassword } = req.body;
+
+  const updatedUser = await userService.updateUser(
+    userId,
+    name,
+    email,
+    oldPassword,
+    newPassword
+  );
+
+  res.status(200).json({
+    success: true,
+    message: "User updated successfully",
+    data: updatedUser,
+  });
+});
--- routes/updateProfile.routes.ts
+++ routes/updateProfile.routes.ts
@@ -3,6 +3,7 @@ import { Router } from "express";
 import { validate } from "../middlewares/validate";
 import { protectedRoute } from "middlewares/protectedRoute";
 import { updateUserSchema } from "validations/userProfile.schema";
+import { updateUser } from "controllers/user.controller";
 
 const router = Router();
 
