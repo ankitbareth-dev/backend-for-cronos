d77e706c5fa51f846551562ce72723abeac20377
ankitbareth-dev <ankitbareth.dev@gmail.com>
2025-11-26 09:18:12 +0530

    matrix routes added

---
+++ routes/matrix.routes.ts
@@ -0,0 +1,36 @@
+import { Router } from "express";
+import { MatrixController } from "../controllers/matrix.controller";
+import { protectedRoute } from "../middlewares/protectedRoute";
+import { validate } from "../middlewares/validate";
+import {
+  createMatrixSchema,
+  editMatrixSchema,
+  deleteMatrixSchema,
+} from "../validations/matrix.schema";
+
+const router = Router();
+
+router.get("/", protectedRoute, MatrixController.getUserMatrix);
+
+router.post(
+  "/create",
+  protectedRoute,
+  validate(createMatrixSchema),
+  MatrixController.createMatrix
+);
+
+router.put(
+  "/edit",
+  protectedRoute,
+  validate(editMatrixSchema),
+  MatrixController.editMatrix
+);
+
+router.delete(
+  "/delete",
+  protectedRoute,
+  validate(deleteMatrixSchema),
+  MatrixController.deleteMatrix
+);
+
+export default router;
