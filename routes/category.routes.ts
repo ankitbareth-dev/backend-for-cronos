f5ec89a1eea7d25c537f978fd06bb62b7ae6dbac
ankitbareth-dev <ankitbareth.dev@gmail.com>
2025-11-26 11:39:34 +0530

    category routes added

---
+++ routes/category.routes.ts
@@ -0,0 +1,42 @@
+import { Router } from "express";
+import { protectedRoute } from "../middlewares/protectedRoute";
+import { validate } from "../middlewares/validate";
+import {
+  getCategoriesSchema,
+  createCategorySchema,
+  editCategorySchema,
+  deleteCategorySchema,
+} from "../validations/category.schema";
+import { CategoryController } from "../controllers/category.controller";
+
+const router = Router();
+
+router.get(
+  "/",
+  protectedRoute,
+  validate(getCategoriesSchema),
+  CategoryController.getCategories
+);
+
+router.post(
+  "/create",
+  protectedRoute,
+  validate(createCategorySchema),
+  CategoryController.createCategory
+);
+
+router.put(
+  "/edit",
+  protectedRoute,
+  validate(editCategorySchema),
+  CategoryController.editCategory
+);
+
+router.delete(
+  "/delete",
+  protectedRoute,
+  validate(deleteCategorySchema),
+  CategoryController.deleteCategory
+);
+
+export default router;
