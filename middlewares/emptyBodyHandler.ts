e5e2b493d10bb8b8a25b2e577bb2b7d4685823c1
ankitbareth-dev <ankitbareth.dev@gmail.com>
2025-12-14 14:32:37 +0530

    any

--- middlewares/emptyBodyHandler.ts
+++ middlewares/emptyBodyHandler.ts
@@ -6,7 +6,7 @@ export const emptyBodyHandler = (
   next: NextFunction
 ) => {
   if (
-    ["POST", "PUT", "PATCH"].includes(req.method) &&
+    ["POST", "PUT"].includes(req.method) &&
     (!req.body || Object.keys(req.body).length === 0)
   ) {
     return res.status(400).json({
