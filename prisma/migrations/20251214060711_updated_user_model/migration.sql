ba7468b4214f3c27aa666d515bf9e912988647d5
Ankit Bareth <ankitbareth.dev@gmail.com>
2026-02-22 21:57:05 +0530

    madesome changes

--- prisma/migrations/20251214060711_updated_user_model/migration.sql
+++
@@ -1,5 +0,0 @@
--- CreateEnum
-CREATE TYPE "AuthProvider" AS ENUM ('GOOGLE');
-
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "provider" "AuthProvider" NOT NULL DEFAULT 'GOOGLE';
--- prisma/migrations/20260119040932_link_category_to_cells/migration.sql
+++
@@ -1,8 +0,0 @@
--- AlterTable
-ALTER TABLE "MatrixCell" ADD COLUMN     "categoryId" TEXT;
-
--- CreateIndex
-CREATE INDEX "MatrixCell_categoryId_idx" ON "MatrixCell"("categoryId");
-
--- AddForeignKey
-ALTER TABLE "MatrixCell" ADD CONSTRAINT "MatrixCell_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "Category"("id") ON DELETE CASCADE ON UPDATE CASCADE;
